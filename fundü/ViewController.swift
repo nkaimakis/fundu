bbec2b9e185d34c5ddc77a42d9f65160bc11129b
Nick Kaimakis <kaimakis@usc.edu>
2017-11-29 21:33:56 -0800

    cleanup in login VC

--- fundü/ViewController.swift
+++ fundü/ViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var titleLabel: UILabel?
-    var usernameField: UITextField?
-    var passwordField: UITextField?
-    var loginButton: UIButton?
+    var titleLabel: UILabel!
+    var usernameField: UITextField!
+    var passwordField: UITextField!
+    var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,23 +28,23 @@ class ViewController: UIViewController {
 
     func createTitleLabel(){
         self.titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
-        titleLabel!.center = CGPoint(x: 50, y: 285)
-        titleLabel!.textAlignment = .center
-        titleLabel!.text = "fundu"
-        titleLabel!.textColor = UIColor.white
-        titleLabel!.center.x = self.view.center.x
-        titleLabel!.center.y = self.view.center.y * 0.5
-        titleLabel!.font = UIFont(name: "DidactGothic-Regular", size: 44)
-        self.view.addSubview(titleLabel!)
+        titleLabel.center = CGPoint(x: 50, y: 285)
+        titleLabel.textAlignment = .center
+        titleLabel.text = "fundu"
+        titleLabel.textColor = UIColor.white
+        titleLabel.center.x = self.view.center.x
+        titleLabel.center.y = self.view.center.y * 0.5
+        titleLabel.font = UIFont(name: "DidactGothic-Regular", size: 44)
+        self.view.addSubview(titleLabel)
     }
     
     func createLoginFields(){
         self.usernameField = getTextFieldWithPlaceholder(placeholder: "Enter username", fontSize: 14)
-        usernameField!.center.y = self.view.center.y * 1
-        self.view.addSubview(usernameField!)
+        usernameField.center.y = self.view.center.y * 1
+        self.view.addSubview(usernameField)
         self.passwordField = getTextFieldWithPlaceholder(placeholder: "Enter password", fontSize: 14)
-        passwordField!.center.y = usernameField!.center.y + 30
-        self.view.addSubview(passwordField!)
+        passwordField!.center.y = usernameField.center.y + 30
+        self.view.addSubview(passwordField)
     }
     
     func getTextFieldWithPlaceholder(placeholder: String, fontSize: Int) -> UITextField{
@@ -63,14 +63,14 @@ class ViewController: UIViewController {
     
     func createLoginButton(){
         self.loginButton = UIButton(frame: CGRect(x: 100, y: 400, width: 100, height: 50))
-        loginButton!.backgroundColor = UIColor.gray
-        loginButton!.layer.cornerRadius = 5
-        loginButton!.center.x = self.view.center.x
-        loginButton!.center.y = self.passwordField!.center.y + 100
-        loginButton!.setTitle("Login", for: .normal)
-        loginButton!.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-        loginButton!.tag = 1
-        self.view.addSubview(loginButton!)
+        loginButton.backgroundColor = UIColor.gray
+        loginButton.layer.cornerRadius = 5
+        loginButton.center.x = self.view.center.x
+        loginButton.center.y = self.passwordField!.center.y + 100
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        loginButton.tag = 1
+        self.view.addSubview(loginButton)
     }
     
     //define actions for various buttons
