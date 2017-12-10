b94f4e2b3cffcb9c3c9fd14cc41b2df3680c32fa
Jordan Coppert <coppert@usc.edu>
2017-12-10 01:25:05 -0800

    Made it so keyboard dismisses when editing finishes on Dash

--- fundü/AuthViewController.swift
+++ fundü/AuthViewController.swift
@@ -103,5 +103,10 @@ class AuthViewController: UIViewController {
             }
         }
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
